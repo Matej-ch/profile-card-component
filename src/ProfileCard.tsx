cbdb16afc4c3ebd555fbad544349ef587f48d0b3
Matej <matejchalachan@gmail.com>
2022-12-18 22:11:20 +0100

    fixed accessibility errors

--- src/ProfileCard.tsx
+++ src/ProfileCard.tsx
@@ -16,7 +16,7 @@ type Props = {
 
 function ProfileCard(props: Props) {
 
-    return (<div className={'profile-card'}>
+    return (<div className={'profile-card'} role="main">
 
         <div className={'background'}>
 
